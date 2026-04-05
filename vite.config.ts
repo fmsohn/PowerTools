ddabf3da7dd2f62bee04edaa652c2cd2503a6d53
Frank Sohn <fmsohn@gmail.com>
2026-04-05 10:07:56 -0500

    Offline First Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: false,
+      includeAssets: ['favicon.svg', 'icons.svg', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'PowerTools',
+        short_name: 'PowerTools',
+        theme_color: '#000000',
+        display: 'standalone',
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+    }),
+  ],
 })
